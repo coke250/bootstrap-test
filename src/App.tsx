712231c29e4de57235105d0e16206664c1d8948c
ssabi <fhy1930@gmail.com>
2021-11-03 21:34:55 +0900

    테스트 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  Button,
+  ProgressBar,
+  ListGroup,
+  Alert,
+  Spinner,
+} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="text-primary">안녕하세요</div>
+      <Button>확인</Button>
+      <ListGroup as="ul">
+        <ListGroup.Item action active variant="primary" as="li">
+          Cras justo odio
+        </ListGroup.Item>
+        <ListGroup.Item variant="primary" as="li">
+          Dapibus ac facilisis in
+        </ListGroup.Item>
+        <ListGroup.Item variant="primary" as="li" disabled>
+          Morbi leo risus
+        </ListGroup.Item>
+        <ListGroup.Item variant="primary" as="li" active>
+          Porta ac consectetur ac
+        </ListGroup.Item>
+      </ListGroup>
+      <ProgressBar variant="primary" now={60} label={`60%`} />
+      <Alert>알림창입니다.</Alert>
+      <Spinner animation="border" role="status" variant="primary">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </Container>
   );
 }
 
